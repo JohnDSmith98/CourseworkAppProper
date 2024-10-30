49d29ca0ee84b254580510add30849e87c55ab8e
JohnDSmith98 <146352311+JohnDSmith98@users.noreply.github.com>
2024-10-30 02:19:20 +0000

    changes. need to restart

--- app/src/main/java/com/example/courseworkappproper/landingpage.java
+++ app/src/main/java/com/example/courseworkappproper/landingpage.java
@@ -1,9 +1,6 @@
 package com.example.courseworkappproper;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    private Button log;
+public class landingpage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        log=findViewById(R.id.loginbutton);
-        log.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent landingpageintent=new Intent(MainActivity.this, landingpage.class);
-                startActivity(landingpageintent);
-            }
-        });
-
+        setContentView(R.layout.activity_landingpage);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
