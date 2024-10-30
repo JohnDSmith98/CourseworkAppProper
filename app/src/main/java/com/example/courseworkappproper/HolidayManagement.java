6a4f5c5075120ae9da856075d03e890118da1602
JohnDSmith98 <146352311+JohnDSmith98@users.noreply.github.com>
2024-10-30 17:11:34 +0000

    company logo added. Holiday management page added. Lots of other changes

--- app/src/main/java/com/example/courseworkappproper/HolidayManagement.java
+++ app/src/main/java/com/example/courseworkappproper/HolidayManagement.java
@@ -10,17 +10,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SettingsPage extends AppCompatActivity {
+public class HolidayManagement extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_settings_page);
-        ListView list = findViewById(R.id.PreferenceList);
-        String[] placeholderText = {"Preference 1", "Preference 2", "Preference 3"};
+        setContentView(R.layout.activity_holiday_management);
+        ListView list = findViewById(R.id.ApprovedList);
+        String[] placeholderText = {"Approval 1", "Approval 2", "Approval 3"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, placeholderText );
         list.setAdapter(adapter);
+        ListView list2 = findViewById(R.id.CurrentReqList);
+        String[] placeholderText2 = {"Approval 1", "Approval 2"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, placeholderText2 );
+        list2.setAdapter(adapter2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/courseworkappproper/landingpage.java
+++ app/src/main/java/com/example/courseworkappproper/landingpage.java
@@ -13,20 +13,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class landingpage extends AppCompatActivity {
     private Button settings;
+    private Button holiday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_landingpage);
+        holiday=findViewById(R.id.HolidayButton);
         settings=findViewById(R.id.settings);
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(landingpage.this,SettingsPage.class);
+                Intent intent = new Intent(landingpage.this, SettingsPage.class);
                 startActivity(intent);
             }
         });
+        holiday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent hols = new Intent(landingpage.this, HolidayManagement.class);
+                startActivity(hols);
+            }
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
