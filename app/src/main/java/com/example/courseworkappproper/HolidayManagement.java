496e97667f30980ad26c794d8543f2f97b0309ad
JohnDSmith98 <146352311+JohnDSmith98@users.noreply.github.com>
2024-10-31 01:01:05 +0000

    Added date picker buttons and request page as a whole.

--- app/src/main/java/com/example/courseworkappproper/HolidayManagement.java
+++ app/src/main/java/com/example/courseworkappproper/HolidayManagement.java
@@ -1,7 +1,10 @@
 package com.example.courseworkappproper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,6 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HolidayManagement extends AppCompatActivity {
+    private Button req;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,14 @@ public class HolidayManagement extends AppCompatActivity {
         String[] placeholderText2 = {"Approval 1", "Approval 2"};
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, placeholderText2 );
         list2.setAdapter(adapter2);
+        req=findViewById(R.id.ReqButton);
+        req.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ReqIntent=new Intent(HolidayManagement.this, RequestPage.class);
+                startActivity(ReqIntent);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
